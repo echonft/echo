e41b59ec7f7b662a8b4d56627658f0761efe4294
Johnny Cage <2409861+0xjohnnycage@users.noreply.github.com>
2022-12-06 04:07:14 -0500

    added react-hooks eslint plugin

--- app/frontend/.eslintrc.js
+++ app/frontend/.eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['plugin:@next/next/recommended', '../../.eslintrc.js'],
+  extends: ['plugin:react-hooks/recommended', 'plugin:@next/next/recommended', '../../.eslintrc.js'],
   rules: {
     'no-restricted-imports': [
       'error',
