3330689f1337db03188787925845cab3a733f2ce
Gabriel Cartier <4060669+GabrielCartier@users.noreply.github.com>
2023-04-10 18:35:05 +0700

    WIP add wallet button

--- app/frontend/src/components/user-profile.tsx
+++ app/frontend/src/components/user-profile.tsx
@@ -1,4 +1,4 @@
-import { useDiscordGuild } from '@echo/firebase-react'
+import { WalletToolbar } from '@components/wallet-toolbar'
 import { useUser } from '@lib/hooks/use-user'
 import { R } from '@mobily/ts-belt'
 import { isNil } from 'ramda'
@@ -6,17 +6,13 @@ import { FunctionComponent } from 'react'
 
 export const UserProfile: FunctionComponent = () => {
   const user = useUser()
-  const { isLoading, data: result } = useDiscordGuild('Y8GBFtPZKElp44z0k10D')
-  console.log(`user result is ${JSON.stringify(user)} discord guild is ${JSON.stringify(result)}`)
   if (isNil(user)) {
     return <>Loading user...</>
   }
   if (R.isError(user)) {
-    R.tapError((error) => console.log(`error from useUser ${error}`))(user)
     return <>Error on user</>
     // TODO Add callback?
     // return <Redirect to={'/login'} />
   }
-  console.log(`will display user`)
-  return <>Got data</>
+  return <WalletToolbar userId={R.getExn(user).id} />
 }
