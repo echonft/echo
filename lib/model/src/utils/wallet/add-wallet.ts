228361d73d66fdf93b712b1d480857d5de1e5daf
Gabriel Cartier <4060669+GabrielCartier@users.noreply.github.com>
2023-04-17 18:29:49 +0700

    Fixed utils

--- lib/model/src/utils/wallet/add-wallet.ts
+++
@@ -1,5 +0,0 @@
-import { walletEquals } from '../../predicates/wallet/wallet-equals'
-import { Wallet } from '../../types/wallet'
-import { addToArrayIfNotPresent } from '@echo/utils'
-export const addWallet = (wallets: Wallet[], walletToAdd: Wallet): Wallet[] =>
-  addToArrayIfNotPresent<Wallet>(wallets, walletToAdd, walletEquals)
--- lib/utils/src/array/remove-from-array-if-present.ts
+++
@@ -1,7 +0,0 @@
-import { reject } from 'ramda'
-
-export const removeFromArrayIfPresent = <T>(
-  array: T[],
-  toRemove: T,
-  equals: (source: T) => (target: T) => boolean
-): T[] => reject(equals(toRemove))(array)
