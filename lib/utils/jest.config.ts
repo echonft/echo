38a64dd785d7e5d6cb3a97e6020a1691bfe7f545
Gabriel Cartier <4060669+GabrielCartier@users.noreply.github.com>
2023-04-20 13:03:35 +0700

    updated workflow

--- lib/utils/jest.config.ts
+++ lib/utils/jest.config.ts
@@ -3,7 +3,15 @@ import type { Config } from 'jest'
 const config: Config = {
   modulePathIgnorePatterns: ['<rootDir>/dist/'],
   collectCoverage: true,
-  reporters: ['default', ['github-actions', { silent: false }], 'summary'],
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputDirectory: '<rootDir>/../../coverage/lib/utils'
+      }
+    ]
+  ],
   coverageDirectory: '<rootDir>/../../coverage/lib/utils'
 }
 export default config
