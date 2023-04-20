0443782816c8ee7bcbea8f42cbcf2d011d5e1ae7
Gabriel Cartier <4060669+GabrielCartier@users.noreply.github.com>
2023-04-20 12:35:31 +0700

    test coverage

--- lib/utils/jest.config.ts
+++ lib/utils/jest.config.ts
@@ -3,6 +3,7 @@ import type { Config } from 'jest'
 const config: Config = {
   modulePathIgnorePatterns: ['<rootDir>/dist/'],
   collectCoverage: true,
-  reporters: ['default', ['github-actions', { silent: false }], 'summary']
+  reporters: ['default', ['github-actions', { silent: false }], 'summary'],
+  coverageDirectory: '<rootDir>/../../coverage/lib/utils'
 }
 export default config
