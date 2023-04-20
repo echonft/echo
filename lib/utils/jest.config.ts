4484fe54444f47d837e177f0b89f90a0b7eeac37
Gabriel Cartier <4060669+GabrielCartier@users.noreply.github.com>
2023-04-20 13:27:04 +0700

    updated files type

--- lib/utils/jest.config.ts
+++ lib/utils/jest.config.ts
@@ -5,12 +5,14 @@ const config: Config = {
   collectCoverage: true,
   reporters: [
     'default',
+    ['github-actions', { silent: false }],
     [
       'jest-junit',
       {
         outputDirectory: '<rootDir>/../../coverage/lib/utils'
       }
-    ]
+    ],
+    'summary'
   ],
   coverageDirectory: '<rootDir>/../../coverage/lib/utils'
 }
