56a791ed2b6f9a0ee163b86be8a716416f3ab213
Johnny Cage <jfcartier@johnnycage.home>
2023-08-10 10:44:26 -0400

    added user discord tag component

---
+++ lib/ui/src/components/skeleton/user/user-discord-tag-skeleton.tsx
@@ -0,0 +1,22 @@
+import { clsx } from 'clsx'
+import { FunctionComponent } from 'react'
+
+export const UserDiscordTagSkeleton: FunctionComponent = () => {
+  return (
+    <div
+      className={clsx(
+        'flex',
+        'flex-row',
+        'h-[2.125rem]',
+        'w-[16rem]',
+        'py-0.25',
+        'px-2.5',
+        'rounded-lg',
+        'items-center',
+        'bg-purple-500',
+        'gap-[0.31rem]',
+        'animate-pulse'
+      )}
+    />
+  )
+}
