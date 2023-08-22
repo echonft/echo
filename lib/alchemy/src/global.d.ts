a4c7cd73998ad813ae6ac6ff1443a5b34fa38f81
Johnny Cage <2409861+0xjohnnycage@users.noreply.github.com>
2023-08-22 05:28:41 -0400

    fix

--- lib/alchemy/src/global.d.ts
+++ lib/alchemy/src/global.d.ts
@@ -1,6 +1,7 @@
 export declare global {
   namespace NodeJS {
     interface ProcessEnv extends Dict<string> {
+      NODE_ENV: 'production' | 'development' | 'test'
       ALCHEMY_API_KEY: string
     }
   }
