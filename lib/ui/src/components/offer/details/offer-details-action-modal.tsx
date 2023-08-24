d049047f5d69c45d8a0ac3af2670e8c4a6f1382a
Gabriel Cartier <4060669+GabrielCartier@users.noreply.github.com>
2023-08-25 00:28:40 +0700

    removed test code?

--- lib/ui/src/components/offer/details/offer-details-action-modal.tsx
+++ lib/ui/src/components/offer/details/offer-details-action-modal.tsx
@@ -11,21 +11,16 @@ export interface OfferDetailsActionModalProps {
 export const OfferDetailsActionModal: FunctionComponent<OfferDetailsActionModalProps> = ({ offerState }) => {
   const [isOpen, setIsOpen] = useState<boolean>(false)
 
-  console.log(`rendering offer details`)
   useEffect(() => {
-    console.log(`will update set is open ${offerState}`)
     if (offerState === 'ACCEPTED' || offerState === 'CANCELLED' || offerState === 'REJECTED') {
-      console.log(`will open`)
       setIsOpen(true)
     } else {
-      console.log(`will close`)
       setIsOpen(false)
     }
   }, [setIsOpen, offerState])
 
   // Should never happen but otherwise typescript complains on the typing
   if (offerState === 'OPEN' || offerState === 'INVALID') {
-    console.log('should not happen')
     return null
   }
 
