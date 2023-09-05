f551ba9c71634cf2eac6d7befc7f293e2ba5ff54
Johnny Cage <2409861+0xjohnnycage@users.noreply.github.com>
2023-09-05 05:35:36 -0400

    fixed make offer btn

--- lib/ui/src/components/collection/collection-offer-button.tsx
+++ lib/ui/src/components/collection/collection-offer-button.tsx
@@ -11,8 +11,9 @@ export const CollectionOfferButton: FunctionComponent<Props> = ({ count, onMakeO
   const t = useTranslations('collection.button.makeOffer')
   return (
     <button
+      disabled={count === 0}
       onClick={onMakeOffer}
-      className={clsx('!justify-between', 'btn-gradient', 'group', 'rounded-lg', 'w-full', 'px-2', 'py-0.5')}
+      className={clsx('!justify-between', 'btn-gradient', 'group', 'rounded-lg', 'w-full', 'p-2.5')}
     >
       <span className={clsx('prose-label-sm-semi', 'btn-label-gradient')}>{t('label')}</span>
       <div className={clsx('flex', 'items-center', 'justify-center', 'w-6', 'h-6', 'rounded-lg', 'bg-dark-300')}>
