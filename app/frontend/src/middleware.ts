aa569713e19b828f4d9c9a5d2c2c98189f54d35d
Johnny Cage Wins <2409861+0xjohnnycage@users.noreply.github.com>
2023-09-07 05:32:32 -0400

    added limit to nft listings query

--- app/frontend/src/middleware.ts
+++ app/frontend/src/middleware.ts
@@ -13,7 +13,8 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
+     * - manifest.webmanifest
      */
-    '/((?!api|_next/static|_next/image|favicon.ico).*)'
+    '/((?!api|_next/static|_next/image|favicon.ico|manifest.webmanifest).*)'
   ]
 }
