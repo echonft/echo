a3329b4a9e5bea107767402aad9305ab15ef4991
Johnny Cage Wins <2409861+0xjohnnycage@users.noreply.github.com>
2023-09-08 01:08:52 -0400

    rebase fixes

--- lib/api/src/routing/collection-swaps-api-url.ts
+++ lib/api/src/routing/collection-swaps-api-url.ts
@@ -1,11 +1,7 @@
 import { apiBaseUrl } from './api-base-url'
 import { isEmpty } from 'ramda'
 
-<<<<<<<< HEAD:lib/api/src/routing/collection-swaps-api-url.ts
 export function collectionSwapsApiUrl(slug: string) {
-========
-export function collectionApiUrl(slug: string) {
->>>>>>>> 586a9717 (added listings to nft details page):lib/api/src/routing/collection-api-url.ts
   if (isEmpty(slug)) {
     throw Error('collection slug is required')
   }
