54665eb17ea268dee88b9caafc289b338de85a9d
Johnny Cage Wins <2409861+0xjohnnycagewins@users.noreply.github.com>
2023-10-10 16:41:13 -0400

    added task to update addresses

--- lib/firestore/utils/add-collection-dev.ts
+++ lib/firestore/utils/add-collection-dev.ts
@@ -3,13 +3,14 @@ import { addNftCollectionDiscordGuild } from '@echo/firestore/crud/nft-collectio
 import { initializeFirebase } from '@echo/firestore/services/initialize-firebase'
 import { terminateFirestore } from '@echo/firestore/services/terminate-firestore'
 import { FirestoreNftCollection } from '@echo/firestore/types/model/nft-collection/firestore-nft-collection'
+import { getAddress } from 'viem'
 
 const collectionToAdd: Omit<FirestoreNftCollection, 'id'> = {
   bannerUrl:
     'https://i.seadn.io/gae/i5dYZRkVCUK97bfprQ3WXyrT9BnLSZtVKGJlKQ919uaUB0sxbngVCioaiyu9r6snqfi2aaTyIvv6DHm4m2R3y7hMajbsv14pSZK8mhs?auto=format&dpr=1&w=2048',
   blurUrl: 'https://blur.io/collection/boredapeyachtclub',
   contract: {
-    address: '0x65426F3C04e85936b0F875510d045b413134186A',
+    address: getAddress('0x65426F3C04e85936b0F875510d045b413134186A'),
     chainId: 11155111,
     name: 'BoredApeYachtClub',
     symbol: 'BAYC',
---
+++ lib/firestore/utils/fix-addresses-dev.ts
@@ -0,0 +1,91 @@
+import { getAllListings } from '@echo/firestore/crud/listing/get-all-listings'
+import { getAllNfts } from '@echo/firestore/crud/nft/get-all-nfts'
+import { getAllNftCollections } from '@echo/firestore/crud/nft-collection/get-all-nft-collections'
+import { getAllOffers } from '@echo/firestore/crud/offer/get-all-offers'
+import { getAllWallets } from '@echo/firestore/crud/wallet/get-all-wallets'
+import { initializeFirebase } from '@echo/firestore/services/initialize-firebase'
+import { terminateFirestore } from '@echo/firestore/services/terminate-firestore'
+import type { FirestoreListing } from '@echo/firestore/types/model/listing/firestore-listing'
+import type { FirestoreNft } from '@echo/firestore/types/model/nft/firestore-nft'
+import type { FirestoreNftCollection } from '@echo/firestore/types/model/nft-collection/firestore-nft-collection'
+import type { FirestoreOffer } from '@echo/firestore/types/model/offer/firestore-offer'
+import { FirestoreWallet } from '@echo/firestore/types/model/wallet/firestore-wallet'
+import { updateNftCollection } from '@echo/firestore-mocks/nft-collection/update-nft-collection'
+import { updateListing } from '@test-utils/listing/update-listing'
+import { updateNft } from '@test-utils/nft/update-nft'
+import { updateOffer } from '@test-utils/offer/update-offer'
+import { updateWallet } from '@test-utils/wallet/update-wallet'
+import { assoc, converge, lens, map, modify, modifyPath, omit, over, pick, pipe, prop } from 'ramda'
+import { getAddress } from 'viem'
+
+const updateAddressLens = lens(pick(['chainId', 'address']), assoc('address'))
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+const updateAddress = over(updateAddressLens, converge(getAddress, [prop('address'), prop('chainId')]))
+
+void (async function () {
+  initializeFirebase()
+  // fix listings addresses
+  const listings = await getAllListings()
+  for (const listing of listings) {
+    const updateData = pipe(
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      modifyPath(['creator', 'wallet'], updateAddress),
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      modify('items', map(modifyPath(['nft', 'owner', 'wallet'], updateAddress))),
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      modify('targets', map(modifyPath(['collection', 'contract'], updateAddress))),
+      omit(['id'])
+    )(listing) as FirestoreListing
+    await updateListing(listing.id, updateData)
+  }
+  // fix nft collections addresses
+  const nftCollections = await getAllNftCollections()
+  for (const nftCollection of nftCollections) {
+    const updateData = pipe(
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      modify('contract', updateAddress),
+      omit(['id'])
+    )(nftCollection) as FirestoreNftCollection
+    await updateNftCollection(nftCollection.id, updateData)
+  }
+  // fix nfts addresses
+  const nfts = await getAllNfts()
+  for (const nft of nfts) {
+    const updateData = pipe(
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      modifyPath(['owner', 'wallet'], updateAddress),
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      modifyPath(['collection', 'contract'], updateAddress),
+      omit(['id'])
+    )(nft) as FirestoreNft
+    await updateNft(nft.id, updateData)
+  }
+  // fix offers addresses
+  const offers = await getAllOffers()
+  for (const offer of offers) {
+    const updateData = pipe(
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      modify('receiverItems', map(modifyPath(['nft', 'owner', 'wallet'], updateAddress))),
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      modify('senderItems', map(modifyPath(['nft', 'owner', 'wallet'], updateAddress))),
+      omit(['id'])
+    )(offer) as FirestoreOffer
+    await updateOffer(offer.id, updateData)
+  }
+  // fix wallets addresses
+  const wallets = await getAllWallets()
+  for (const wallet of wallets) {
+    const updateData = pipe(updateAddress, omit(['id']))(wallet) as FirestoreWallet
+    await updateWallet(wallet.id, updateData)
+  }
+  await terminateFirestore()
+})()
