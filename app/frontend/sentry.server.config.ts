191fd5daee75492331482d1565ca6fd4d8124eb1
Johnny Cage Wins <2409861+0xjohnnycagewins@users.noreply.github.com>
2023-11-01 06:05:24 -0400

    updated packages

--- app/frontend/sentry.server.config.ts
+++ app/frontend/sentry.server.config.ts
@@ -7,6 +7,9 @@ import * as Sentry from '@sentry/nextjs'
 Sentry.init({
   dsn: 'https://90f90a5ace372a2805407eeeb7d7fc15@o4506149604098048.ingest.sentry.io/4506149609472000',
 
+  // Ignore DynamicServerError errors when building
+  ignoreErrors: ['Dynamic server usage'],
+
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
 
--- app/frontend/src/app/layout.tsx
+++ app/frontend/src/app/layout.tsx
@@ -1,18 +1,21 @@
 import '@echo/ui-css/index.css'
-import { type Metadata } from 'next'
+import { type Metadata, type Viewport } from 'next'
 import { type FunctionComponent, type PropsWithChildren } from 'react'
 
 export const metadata: Metadata = {
   title: 'Echo',
   description: 'Echo',
   applicationName: 'Echo',
-  viewport: {
-    width: 'device-width',
-    initialScale: 1
-  },
   other: {
     charset: 'utf-8'
   }
+}
+export const viewport: Viewport = {
+  colorScheme: 'dark',
+  initialScale: 1,
+  maximumScale: 1,
+  themeColor: '#121212',
+  width: 'device-width'
 }
 
 const Layout: FunctionComponent<PropsWithChildren> = ({ children }) => {
