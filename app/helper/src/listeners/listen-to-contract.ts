070464c2d13bd3da22a1125a89cfe29f956ac1f4
Gabriel Cartier <4060669+GabrielCartier@users.noreply.github.com>
2023-11-09 18:17:53 -0500

    completed contract listener

--- app/helper/src/listeners/listen-to-contract.ts
+++ app/helper/src/listeners/listen-to-contract.ts
@@ -1,8 +1,10 @@
-import { logger } from '@echo/utils/services/logger'
+import { tradeExecutedHandler } from '@echo/helper/handlers/trade-executed-handler'
 import { ECHO_ABI } from '@echo/web3/constants/echo-abi'
 import { getChainId } from '@echo/web3/helpers/get-chain-id'
 import { getEchoAddress } from '@echo/web3/helpers/get-echo-address'
 import { getViemClient } from '@echo/web3/helpers/get-viem-client'
+import { forEach, isNotNil } from 'ramda'
+import { decodeEventLog, type Log } from 'viem'
 
 export function listenToContract() {
   const chainId = getChainId()
@@ -11,9 +13,16 @@ export function listenToContract() {
     address: getEchoAddress(chainId),
     abi: ECHO_ABI,
     eventName: 'TradeExecuted',
-    onLogs: (logs) => {
-      logger.info('Got contract event')
-      logger.info(JSON.stringify(logs))
-    }
+    onLogs: forEach((log: Log) => {
+      const decodedLog = decodeEventLog({
+        abi: ECHO_ABI,
+        eventName: 'TradeExecuted',
+        data: log.data,
+        topics: log.topics
+      })
+      if (isNotNil(log.transactionHash)) {
+        void tradeExecutedHandler(decodedLog.args.id, log.transactionHash)
+      }
+    })
   })
 }
