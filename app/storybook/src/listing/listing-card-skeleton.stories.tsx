ced778c4245163b5103d64fa44bc375a2dcf20cc
Johnny Cage Wins <2409861+0xjohnnycagewins@users.noreply.github.com>
2023-12-22 02:49:18 -0500

    added listing card

---
+++ app/storybook/src/listing/listing-card-skeleton.stories.tsx
@@ -0,0 +1,13 @@
+import { ListingCardSkeleton as Component } from '@echo/ui/components/listing/card/skeleton/listing-card-skeleton'
+import { type Meta, type StoryObj } from '@storybook/react'
+
+const metadata: Meta<typeof Component> = {
+  title: 'Listing/Card',
+  component: Component
+}
+
+export default metadata
+
+type Story = StoryObj<typeof Component>
+
+export const Skeleton: Story = {}
---
+++ lib/ui/src/components/listing/card/layout/listing-card-layout.tsx
@@ -0,0 +1,33 @@
+import type { WithChildrenProps } from '@echo/ui/types/props/with-children-props'
+import type { WithClassNameProps } from '@echo/ui/types/props/with-class-name-props'
+import type { WithLoadingProps } from '@echo/ui/types/props/with-loading-props'
+import { clsx } from 'clsx'
+import type { FunctionComponent, MouseEventHandler } from 'react'
+
+interface Props extends WithChildrenProps, WithLoadingProps, WithClassNameProps {
+  onClick?: MouseEventHandler
+}
+
+export const ListingCardLayout: FunctionComponent<Props> = ({ onClick, loading, className, children }) => {
+  return (
+    <div
+      className={clsx(
+        'rounded-2xl',
+        'w-[12.625rem]',
+        'h-max',
+        'overflow-clip',
+        'border',
+        'border-solid',
+        'border-white/10',
+        'bg-dark-500',
+        'group',
+        'cursor-pointer',
+        loading && 'animate-pulse',
+        className
+      )}
+      onClick={onClick}
+    >
+      {children}
+    </div>
+  )
+}
