c44610dabcdbd9915e9d480f3e21108f2758fc81
Johnny Cage Wins <2409861+0xjohnnycagewins@users.noreply.github.com>
2024-01-09 23:54:08 -0500

    updated sentry

--- lib/ui/src/components/nft/filters/layout/selectable-nft-groups-and-filters-container.tsx
+++ lib/ui/src/components/nft/filters/layout/selectable-nft-groups-and-filters-container.tsx
@@ -46,13 +46,11 @@ export const SelectableNftGroupsAndFiltersContainer: FunctionComponent<Props> = 
   onButtonClick
 }) => {
   const [groups, setGroups] = useState<Group<SelectableNft>[]>(groupNftsByCollection(nfts))
-
   const { senderItems } = useNewOfferStore()
   // Reset state when offer changes
   useEffect(() => {
     setGroups((prevState) => unselectNftGroupsFromItems(prevState, senderItems))
   }, [senderItems])
-
   const [collectionFilters, setCollectionFilters] = useState(
     includes(NFT_FILTER_COLLECTIONS, availableFilters) ? getCollectionFiltersForNfts(nfts) : []
   )
