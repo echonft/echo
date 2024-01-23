1d09106da7ff79d749f5fd46b44b8284d7e9d17a
Gabriel Cartier <4060669+GabrielCartier@users.noreply.github.com>
2024-01-23 16:27:24 +0700

    Updated profile picture to include a third size

--- lib/ui/src/components/collection/details/collection-profile-picture.tsx
+++ lib/ui/src/components/collection/details/collection-profile-picture.tsx
@@ -1,6 +1,8 @@
-import { ProfilePicture } from '@echo/ui/components/base/profile-picture'
 import { DEFAULT_COLLECTION_PROFILE_PICTURE_URL } from '@echo/ui/constants/default-collection-profile-picture-url'
-import { SIZE_MD } from '@echo/ui/constants/size'
+import { ProfilePicture } from '@echo/ui/components/base/profile-picture'
+import { SIZE_LG, SIZE_MD, SIZE_SM } from '@echo/ui/constants/size'
+import { getProfilePictureHeightInPx } from '@echo/ui/helpers/get-profile-picture-height-in-px'
+import { getProfilePictureWidthInPx } from '@echo/ui/helpers/get-profile-picture-width-in-px'
 import type { ProfilePictureSize } from '@echo/ui/types/profile-picture-size'
 import { type FunctionComponent } from 'react'
 
@@ -8,18 +10,21 @@ export interface CollectionProfilePictureProps {
   collectionName: string
   pictureUrl: string | undefined
   size?: ProfilePictureSize
+  border?: boolean
 }
 
 export const CollectionProfilePicture: FunctionComponent<CollectionProfilePictureProps> = ({
   collectionName,
   pictureUrl,
-  size = SIZE_MD
+  size = SIZE_LG,
+  border = true
 }) => {
   return (
     <ProfilePicture
       pictureUrl={pictureUrl ?? DEFAULT_COLLECTION_PROFILE_PICTURE_URL}
       alt={collectionName}
       size={size}
+      border={border}
     />
   )
 }
