5a2811c0dfb354330dbd04c667f049d6759cc9d7
Gabriel Cartier <4060669+GabrielCartier@users.noreply.github.com>
2024-01-23 16:27:24 +0700

    Updated profile picture to include a third size

---
+++ app/storybook/src/base/shared/profile-picture.stories.tsx
@@ -0,0 +1,34 @@
+// noinspection JSUnusedGlobalSymbols
+
+import { ProfilePicture as Component } from '@echo/ui/components/shared/profile-picture'
+import { SIZE_LG } from '@echo/ui/constants/size'
+import { type Meta, type StoryObj } from '@storybook/react'
+
+const metadata: Meta<typeof Component> = {
+  title: 'Base/Shared/Profile Picture',
+  component: Component,
+  parameters: {
+    controls: {
+      exclude: ['pictureUrl', 'alt']
+    }
+  },
+  argTypes: {
+    size: {
+      options: ['Small', 'Medium', 'Large'],
+      control: { type: 'radio' }
+    }
+  }
+}
+
+export default metadata
+
+type Story = StoryObj<typeof Component>
+
+export const ProfilePicture: Story = {
+  args: {
+    size: SIZE_LG,
+    border: true,
+    alt: 'johnnycagewins',
+    pictureUrl: 'https://cdn.discordapp.com/avatars/462798252543049728/6b3df6d9a8b5ab523fa24a71aca8160d.png'
+  }
+}
--- lib/ui/src/components/base/profile-picture.tsx
+++ lib/ui/src/components/base/profile-picture.tsx
@@ -1,4 +1,6 @@
-import { SIZE_MD, SIZE_SM } from '@echo/ui/constants/size'
+import { SIZE_LG, SIZE_MD, SIZE_SM } from '@echo/ui/constants/size'
+import { getProfilePictureHeightInPx } from '@echo/ui/helpers/get-profile-picture-height-in-px'
+import { getProfilePictureWidthInPx } from '@echo/ui/helpers/get-profile-picture-width-in-px'
 import type { ProfilePictureSize } from '@echo/ui/types/profile-picture-size'
 import { clsx } from 'clsx'
 import Image from 'next/image'
@@ -8,23 +10,26 @@ interface Props {
   pictureUrl: string
   alt: string
   size?: ProfilePictureSize
+  border?: boolean
 }
 
-export const ProfilePicture: FunctionComponent<Props> = ({ pictureUrl, alt, size = SIZE_MD }) => {
+export const ProfilePicture: FunctionComponent<Props> = ({ pictureUrl, alt, size = SIZE_LG, border = true }) => {
   return (
     <Image
       className={clsx(
-        'rounded-2xl',
-        'border-solid',
-        'border-3',
-        'border-yellow-500',
-        size === SIZE_MD && ['h-40', 'w-40'],
-        size === SIZE_SM && ['h-28', 'w-28']
+        (size === SIZE_LG || size === SIZE_MD) && 'rounded-2xl',
+        size === SIZE_SM && 'rounded-lg',
+        border && 'border-solid',
+        border && 'border-3',
+        border && 'border-yellow-500',
+        size === SIZE_LG && ['h-40', 'w-40'],
+        size === SIZE_MD && ['h-28', 'w-28'],
+        size === SIZE_SM && ['h-[6.1875rem]', 'w-[5.875rem]']
       )}
       src={pictureUrl}
       alt={alt}
-      width={size === SIZE_MD ? 160 : 112}
-      height={size === SIZE_MD ? 160 : 112}
+      width={getProfilePictureWidthInPx(size)}
+      height={getProfilePictureHeightInPx(size)}
     />
   )
 }
