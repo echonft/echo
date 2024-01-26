cabe6887bf735303dc8e8c0ae93eca30cdf2cb86
Johnny Cage Wins <2409861+0xjohnnycagewins@users.noreply.github.com>
2024-01-26 13:46:54 -0500

    small improvement

--- lib/ui/src/components/listing/new/new-listing-confirmed-modal.tsx
+++ lib/ui/src/components/listing/new/new-listing-confirmed-modal.tsx
@@ -1,18 +1,15 @@
 'use client'
-import { linkProvider } from '@echo/api/services/routing/link-provider'
 import type { Listing } from '@echo/model/types/listing'
-import { InternalLink } from '@echo/ui/components/base/internal-link'
 import { Modal } from '@echo/ui/components/base/modal/modal'
-import { ConfirmationIconSvg } from '@echo/ui/components/base/svg/confirmation-icon-svg'
-import { clsx } from 'clsx'
+import { NewListingConfirmedModalBody } from '@echo/ui/components/listing/new/new-listing-confirmed-modal-body'
+import type { EmptyFunction } from '@echo/utils/types/empty-function'
 import { useTranslations } from 'next-intl'
-import { isNil } from 'ramda'
 import { type FunctionComponent } from 'react'
 
 interface Props {
   listing: Listing | undefined
   open: boolean
-  onClose?: () => unknown
+  onClose?: EmptyFunction
 }
 
 export const NewListingConfirmedModal: FunctionComponent<Props> = ({ listing, open, onClose }) => {
@@ -20,22 +17,7 @@ export const NewListingConfirmedModal: FunctionComponent<Props> = ({ listing, op
 
   return (
     <Modal open={open} onClose={onClose} title={t('title')}>
-      <div className={clsx('flex', 'flex-col', 'gap-6')}>
-        <span className={clsx('text-white/50', 'text-center', 'prose-header-xs')}>{t('subtitle')}</span>
-        <div className={clsx('flex', 'items-center', 'justify-center')}>
-          <ConfirmationIconSvg />
-        </div>
-        <div className={clsx('flex', 'flex-row', 'gap-4', 'items-center', 'justify-center')}>
-          <InternalLink path={isNil(listing) ? '#' : linkProvider.listing.details.getUrl({ listingId: listing.id })}>
-            <button className={clsx('btn-action', 'btn-size-alt', 'group')}>
-              <span className={clsx('prose-label-lg', 'btn-label-action')}>{t('viewBtn')}</span>
-            </button>
-          </InternalLink>
-          <button className={clsx('btn-gradient', 'btn-size-alt', 'group')} onClick={onClose}>
-            <span className={clsx('prose-label-lg', 'btn-label-gradient')}>{t('closeBtn')}</span>
-          </button>
-        </div>
-      </div>
+      <NewListingConfirmedModalBody listing={listing} onClose={onClose} />
     </Modal>
   )
 }
