3107680a07fe83d6b8c56c594a0a2f878ca0718b
Gabriel Cartier <4060669+GabrielCartier@users.noreply.github.com>
2024-01-29 17:51:01 +0700

    Made the offers on home page use new design

--- app/frontend/src/app/page.tsx
+++ app/frontend/src/app/page.tsx
@@ -2,16 +2,17 @@ import { getCompletedOffers } from '@echo/firestore/crud/offer/get-completed-off
 import { withLocale } from '@echo/frontend/lib/decorators/with-locale'
 import { withUser } from '@echo/frontend/lib/decorators/with-user'
 import { getCollectionsWithSwapsCount } from '@echo/frontend/lib/helpers/collection/get-collections-with-swaps-count'
+import { setOfferRoleForUser } from '@echo/frontend/lib/helpers/offer/set-offer-role-for-user'
 import type { NextUserParams } from '@echo/frontend/lib/types/next-user-params'
 import { PageLayout } from '@echo/ui/components/base/layout/page-layout'
 import { PAGE_LAYOUT_BG_HOME } from '@echo/ui/constants/page-layout-background'
 import { HomePage } from '@echo/ui/pages/home/home-page'
-import { pipe } from 'ramda'
+import { andThen, map, pipe } from 'ramda'
 import type { ReactElement } from 'react'
 
 async function render({ user }: NextUserParams) {
   const collections = await getCollectionsWithSwapsCount(10)
-  const offers = await getCompletedOffers(5)
+  const offers = await pipe(getCompletedOffers, andThen(map(setOfferRoleForUser(user))))(5)
   return (
     <PageLayout user={user} background={PAGE_LAYOUT_BG_HOME}>
       <HomePage collections={collections} offers={offers} />
