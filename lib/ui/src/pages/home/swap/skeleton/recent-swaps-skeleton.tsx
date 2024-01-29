e59d840e05c443de9a9cdfd826a68976194df1b4
Gabriel Cartier <4060669+GabrielCartier@users.noreply.github.com>
2024-01-29 18:00:39 +0700

    Added skeleton view

--- lib/ui/src/pages/home/swap/skeleton/recent-swaps-skeleton.tsx
+++ lib/ui/src/pages/home/swap/skeleton/recent-swaps-skeleton.tsx
@@ -1,6 +1,5 @@
-import { SwapRowSkeleton } from '@echo/ui/components/swap/row/skeleton/swap-row-skeleton'
+import { OfferCardsContainerSkeleton } from '@echo/ui/components/offer/card/layout/skeleton/offer-cards-container-skeleton'
 import { HomeSectionLayout } from '@echo/ui/pages/home/layout/home-section-layout'
-import { RecentSwapsLayout } from '@echo/ui/pages/home/swap/layout/recent-swaps-layout'
 import { useTranslations } from 'next-intl'
 import { type FunctionComponent } from 'react'
 
@@ -8,11 +7,7 @@ export const RecentSwapsSkeleton: FunctionComponent = () => {
   const t = useTranslations('home.recentSwaps')
   return (
     <HomeSectionLayout title={t('title')}>
-      <RecentSwapsLayout>
-        <SwapRowSkeleton />
-        <SwapRowSkeleton />
-        <SwapRowSkeleton />
-      </RecentSwapsLayout>
+      <OfferCardsContainerSkeleton />
     </HomeSectionLayout>
   )
 }
