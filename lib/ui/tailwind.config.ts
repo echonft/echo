7f684fcb7a50699cb1f42e4903d7a6a5f4fa19ef
Johnny Cage Wins <2409861+0xjohnnycagewins@users.noreply.github.com>
2024-02-09 08:36:15 -0500

    renamed cjs files

--- lib/ui/tailwind.config.ts
+++ lib/ui/tailwind.config.ts
@@ -1,4 +1,5 @@
-import { themeExtension } from '@echo/ui/helpers/theme/theme'
+// eslint-disable-next-line no-restricted-imports
+import { themeExtension } from './src/helpers/theme/theme'
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
