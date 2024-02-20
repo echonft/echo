58a75244e7ebc8e76264d5fcf15079fa30d4ae13
Johnny Cage Wins <2409861+0xjohnnycagewins@users.noreply.github.com>
2024-02-19 19:09:30 -0500

    fixes

--- app/frontend/src/lib/request-handlers/offer/cancel-offer-request-handler.ts
+++ app/frontend/src/lib/request-handlers/offer/cancel-offer-request-handler.ts
@@ -1,5 +1,6 @@
 import { ApiRequest } from '@echo/api/types/api-request'
 import type { OfferResponse } from '@echo/api/types/responses/offer-response'
+import { cancelOffer } from '@echo/firestore/crud/offer/cancel-offer'
 import { findOfferById } from '@echo/firestore/crud/offer/find-offer-by-id'
 import { ErrorStatus } from '@echo/frontend/lib/constants/error-status'
 import { guardAsyncFn } from '@echo/frontend/lib/helpers/error/guard'
@@ -8,7 +9,6 @@ import { guarded_assertOfferSenderIs } from '@echo/frontend/lib/helpers/offer/as
 import { guarded_assertOfferState } from '@echo/frontend/lib/helpers/offer/assert/guarded_assert-offer-state'
 import { OFFER_STATE_CANCELLED } from '@echo/model/constants/offer-states'
 import type { AuthUser } from '@echo/model/types/auth-user'
-import { pinoLogger } from '@echo/utils/services/pino-logger'
 import { NextResponse } from 'next/server'
 
 export async function cancelOfferRequestHandler(user: AuthUser, _req: ApiRequest<never>, params: { id: string }) {
@@ -17,11 +17,9 @@ export async function cancelOfferRequestHandler(user: AuthUser, _req: ApiRequest
   guarded_assertOffer(offer)
   guarded_assertOfferState(offer, OFFER_STATE_CANCELLED)
   guarded_assertOfferSenderIs(offer, user.username)
-  // const updatedOffer = await guardAsyncFn(
-  //   cancelOffer,
-  //   ErrorStatus.SERVER_ERROR
-  // )({ offerId: id, updateArgs: { trigger: { by: user.username } } })
-  // return NextResponse.json<OfferResponse>({ offer: updatedOffer })
-  pinoLogger.warn(`trying to cancel offer from cancelOfferRequestHandler`)
-  return NextResponse.json<OfferResponse>({ offer })
+  const updatedOffer = await guardAsyncFn(
+    cancelOffer,
+    ErrorStatus.SERVER_ERROR
+  )({ offerId: id, updateArgs: { trigger: { by: user.username } } })
+  return NextResponse.json<OfferResponse>({ offer: updatedOffer })
 }
--- lib/firestore/test/@utils/offer-update/delete-offer-update.ts
+++ lib/firestore/test/@utils/offer-update/delete-offer-update.ts
@@ -1,8 +1,7 @@
 import { getOfferUpdatesCollectionReference } from '@echo/firestore/helpers/collection-reference/get-offer-updates-collection-reference'
 import { deleteReference } from '@echo/firestore/helpers/crud/reference/delete-reference'
-import { WriteResult } from 'firebase-admin/firestore'
 import { pipe } from 'ramda'
 
-export function deleteOfferUpdate(id: string): Promise<WriteResult> {
+export function deleteOfferUpdate(id: string): Promise<string> {
   return pipe(getOfferUpdatesCollectionReference, deleteReference(id))()
 }
