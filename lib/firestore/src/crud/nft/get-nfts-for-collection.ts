647d36b93d67c8e001deafe90de1de98cea62a29
Gabriel Cartier <4060669+GabrielCartier@users.noreply.github.com>
2024-02-22 20:12:30 +0700

    Fixed DEV-243

--- lib/firestore/src/crud/nft/get-nfts-for-collection.ts
+++ lib/firestore/src/crud/nft/get-nfts-for-collection.ts
@@ -3,12 +3,19 @@ import { getQueryData } from '@echo/firestore/helpers/crud/query/get-query-data'
 import { queryOrderBy } from '@echo/firestore/helpers/crud/query/query-order-by'
 import { queryWhere } from '@echo/firestore/helpers/crud/query/query-where'
 import type { Nft } from '@echo/model/types/nft'
-import { pipe } from 'ramda'
+import { isNil, pipe, unless } from 'ramda'
 
-export function getNftsForCollection(slug: string): Promise<Nft[]> {
+interface GetNftsForCollectionOptions {
+  excludeDiscordUsername?: string
+}
+export function getNftsForCollection(slug: string, options?: GetNftsForCollectionOptions): Promise<Nft[]> {
   return pipe(
     getNftsCollectionReference,
     queryWhere<Nft>('collection.slug', '==', slug),
+    unless(
+      () => isNil(options?.excludeDiscordUsername),
+      queryWhere<Nft>('owner.discord.username', '!=', options?.excludeDiscordUsername)
+    ),
     queryOrderBy<Nft>('owner.discord.username'),
     queryOrderBy<Nft>('tokenId'),
     getQueryData
