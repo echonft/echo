b5ef4d668dacec52ab932bf7b8013ac95bc9bc13
Johnny Cage Wins <2409861+0xjohnnycagewins@users.noreply.github.com>
2024-04-10 06:11:27 -0400

    swapping done

--- lib/solana/src/commands/test.ts
+++ lib/solana/src/commands/test.ts
@@ -2,7 +2,7 @@ import { createUmiInstance } from '@echo/solana/helpers/create-umi-instance'
 import { setPayer } from '@echo/solana/helpers/set-payer'
 import { setSigner } from '@echo/solana/helpers/set-signer'
 import { cloneNft } from '@echo/solana/services/clone-nft'
-import { createEchoProgram, createOffer, ECHO_PROGRAM_ID } from '@echo/solana/types/sdk'
+import { acceptOffer, createEchoProgram, createOffer, ECHO_PROGRAM_ID } from '@echo/solana/types/sdk'
 import { pinoLogger } from '@echo/utils/services/pino-logger'
 import {
   findAssociatedTokenPda,
@@ -38,25 +38,26 @@ void (async function () {
   })
   setSigner(umi, process.env.SOLONA_SIGNER_TEST_OFFER_SENDER_PRIVATE_KEY)
   umi.programs.add(createEchoProgram())
-  const senderEscrowAuthority = umi.eddsa.findPda(ECHO_PROGRAM_ID, [
+  const senderEscrow = umi.eddsa.findPda(ECHO_PROGRAM_ID, [
     new TextEncoder().encode('escrow'),
     publicKeyBytes(sender),
     publicKeyBytes(senderTokenMint)
   ])
   const senderEscrowAssociatedTokenAccount = findAssociatedTokenPda(umi, {
     mint: senderTokenMint,
-    owner: publicKey(head(senderEscrowAuthority))
+    owner: publicKey(head(senderEscrow))
   })
   const offer = umi.eddsa.findPda(ECHO_PROGRAM_ID, [
     new TextEncoder().encode('offer'),
     publicKeyBytes(sender),
-    publicKeyBytes(senderTokenMint)
+    publicKeyBytes(receiver)
   ])
+
   pinoLogger.info(`sender: ${sender}`)
   pinoLogger.info(`sender token: ${senderToken.publicKey}`)
   pinoLogger.info(`sender tokenMint: ${senderTokenMint}`)
-  pinoLogger.info(`sender escrow authority: ${head(senderEscrowAuthority)}`)
-  pinoLogger.info(`sender escrow authority bump: ${last(senderEscrowAuthority)}`)
+  pinoLogger.info(`sender escrow authority: ${head(senderEscrow)}`)
+  pinoLogger.info(`sender escrow authority bump: ${last(senderEscrow)}`)
   pinoLogger.info(`sender escrow associated token account: ${head(senderEscrowAssociatedTokenAccount)}`)
   pinoLogger.info(`sender escrow associated token account bump: ${last(senderEscrowAssociatedTokenAccount)}`)
   pinoLogger.info(`offer: ${head(offer)}`)
@@ -64,20 +65,43 @@ void (async function () {
   pinoLogger.info(`receiver: ${receiver}`)
   pinoLogger.info(`receiver token: ${receiverToken.publicKey}`)
   pinoLogger.info(`receiver tokenMint: ${receiverTokenMint}`)
-  const result = await createOffer(umi, {
+
+  const createOfferResult = await createOffer(umi, {
     sender: umi.identity,
     token: senderToken.publicKey,
     tokenMint: senderTokenMint,
-    escrow: senderEscrowAuthority,
+    escrow: senderEscrow,
     escrowAssociatedTokenAccount: senderEscrowAssociatedTokenAccount,
     offer,
-    receiver: receiver,
+    receiver,
     receiverToken: receiverToken.publicKey,
     receiverTokenMint: receiverTokenMint,
     splTokenProgram: SPL_TOKEN_PROGRAM_ID,
-    systemProgram: publicKey(SystemProgram.programId),
-    associatedTokenProgram: SPL_ASSOCIATED_TOKEN_PROGRAM_ID
+    associatedTokenProgram: SPL_ASSOCIATED_TOKEN_PROGRAM_ID,
+    systemProgram: publicKey(SystemProgram.programId)
   }).sendAndConfirm(umi, { send: { skipPreflight: true } })
-  pinoLogger.info(`Done ${JSON.stringify(result.result)}`)
+  pinoLogger.info(`offer created ${JSON.stringify(createOfferResult.result)}`)
+
+  const receiverAssociatedTokenAccount = findAssociatedTokenPda(umi, { mint: senderTokenMint, owner: receiver })
+  const senderAssociatedTokenAccount = findAssociatedTokenPda(umi, { mint: receiverTokenMint, owner: sender })
+
+  setSigner(umi, process.env.SOLONA_SIGNER_TEST_OFFER_RECEIVER_PRIVATE_KEY)
+  const acceptOfferResult = await acceptOffer(umi, {
+    receiver: umi.identity,
+    token: receiverToken.publicKey,
+    tokenMint: receiverTokenMint,
+    asociatedTokenAccount: receiverAssociatedTokenAccount,
+    offer,
+    sender,
+    senderEscrow,
+    senderEscrowAssociatedTokenAccount,
+    senderTokenMint,
+    senderAssociatedTokenAccount,
+    splTokenProgram: SPL_TOKEN_PROGRAM_ID,
+    associatedTokenProgram: SPL_ASSOCIATED_TOKEN_PROGRAM_ID,
+    systemProgram: publicKey(SystemProgram.programId)
+  }).sendAndConfirm(umi, { send: { skipPreflight: true } })
+  pinoLogger.info(`offer accepted: swap done ${JSON.stringify(acceptOfferResult.result)}`)
+
   process.exit()
 })()
