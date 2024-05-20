82be7e59f079606841f8512d84e09a6b04ab06bf
Gabriel Cartier <4060669+GabrielCartier@users.noreply.github.com>
2024-05-20 15:51:17 -0400

    WIP (not working)

--- app/storybook/src/mocks/create-offer.ts
+++ app/storybook/src/mocks/create-offer.ts
@@ -4,7 +4,8 @@ import { getOfferMockById } from '@echo/model-mocks/offer/get-offer-mock-by-id'
 import { OFFER_MOCK_TO_JOHNNYCAGE_ID } from '@echo/model-mocks/offer/offer-mock'
 import { toPromise } from '@echo/utils/fp/to-promise'
 import { delayPromise } from '@echo/utils/helpers/delay-promise'
-import { applySpec, pipe } from 'ramda'
+import type { HexString } from '@echo/utils/types/hex-string'
+import type { CreateOfferArgs } from '@echo/web3-dom/types/create-offer-args'
 
 export function createOffer(_args: CreateOfferRequest): Promise<OfferResponse> {
   return delayPromise(
