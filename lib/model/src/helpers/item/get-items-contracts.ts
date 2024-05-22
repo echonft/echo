bad1ff16f130c1bbf7bb7771657e63151b648b05
Gabriel Cartier <4060669+GabrielCartier@users.noreply.github.com>
2024-05-22 19:22:55 -0400

    model done

--- lib/model/src/helpers/item/get-items-contracts.ts
+++
@@ -1,8 +0,0 @@
-import type { Contract } from '@echo/model/types/collection'
-import type { Item } from '@echo/model/types/item'
-import { nonNullableReturn } from '@echo/utils/fp/non-nullable-return'
-import { map, path } from 'ramda'
-
-export function getItemsContracts(offerItems: Item[]): Contract[] {
-  return map<Item, Contract>(nonNullableReturn(path(['nft', 'collection', 'contract'])), offerItems)
-}
