0c91f76c0921a56997a73a2cc7ca15c4c9c700ca
Johnny Cage Wins <2409861+0xjohnnycagewins@users.noreply.github.com>
2024-05-27 17:31:38 -0400

    fixed test

--- lib/opensea/test/mappers/map-collection-response.test.ts
+++ lib/opensea/test/mappers/map-collection-response.test.ts
@@ -1,14 +1,22 @@
+import type { Contract } from '@echo/model/types/collection'
 import { COLLECTION_MOCK_SPIRAL_ID } from '@echo/model-mocks/collection/collection-mock'
 import { getCollectionMockById } from '@echo/model-mocks/collection/get-collection-mock-by-id'
 import { mapCollectionResponse } from '@echo/opensea/mappers/map-collection-response'
 import type { CollectionResponse } from '@echo/opensea/types/response/collection-response'
-import { TESTNET_CHAIN_SEPOLIA } from '@echo/utils/constants/chain-names'
+import { getSupportedChains } from '@echo/utils/helpers/get-supported-chains'
+import type { ChainName } from '@echo/utils/types/chain-name'
+import type { NonEmptyArray } from '@echo/utils/types/non-empty-array'
 import { describe, expect, it } from '@jest/globals'
-import { assoc, omit, pipe } from 'ramda'
+import { assoc, head, modify, omit, pipe } from 'ramda'
 
 describe('mappers - mapCollectionResponse', () => {
   it('maps correctly', () => {
-    const mock = getCollectionMockById(COLLECTION_MOCK_SPIRAL_ID)
+    const supportedChain = pipe<[], NonEmptyArray<ChainName>, ChainName>(getSupportedChains, head)()
+    const unsupportedChain = 'unsupported' as ChainName
+    const mock = pipe(
+      getCollectionMockById,
+      modify<'contract', Contract, Contract>('contract', assoc('chain', supportedChain))
+    )(COLLECTION_MOCK_SPIRAL_ID)
     const response: CollectionResponse = {
       collection: mock.slug,
       name: mock.name,
@@ -29,7 +37,7 @@ describe('mappers - mapCollectionResponse', () => {
       telegram_url: 'whatever',
       twitter_username: 'whatever',
       instagram_username: 'whatever',
-      contracts: [assoc('chain', TESTNET_CHAIN_SEPOLIA, mock.contract), mock.contract],
+      contracts: [assoc('chain', unsupportedChain, mock.contract), mock.contract],
       total_supply: mock.totalSupply,
       created_date: 'whatever'
     }
