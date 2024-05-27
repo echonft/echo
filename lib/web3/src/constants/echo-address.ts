2f5239003a9d27397d904fbd767f06d18ac9d85a
Gabriel Cartier <4060669+GabrielCartier@users.noreply.github.com>
2024-05-27 06:13:30 -0400

    updated contract

--- lib/web3/src/constants/echo-address.ts
+++ lib/web3/src/constants/echo-address.ts
@@ -5,7 +5,7 @@ import type { HexString } from '@echo/utils/types/hex-string'
 export function echoAddressByChain(chain: ChainName): HexString {
   switch (chain) {
     case TESTNET_CHAIN_SEPOLIA:
-      return '0xf7f19bf282a2260940c910F88eC70BbcF51Cf572'
+      return '0x76FBe909020c1B133b3Be0347Fe4136734D3ac8a'
     default:
       throw Error(`chain ${chain} not supported`)
   }
