03133d009829b561b2280d4391040f4646f2443c
Johnny Cage Wins <2409861+0xjohnnycagewins@users.noreply.github.com>
2024-05-27 03:19:22 -0400

    fixed test

--- lib/model/test/helpers/nft/get-open-sea-url-for-nft.test.ts
+++ lib/model/test/helpers/nft/get-open-sea-url-for-nft.test.ts
@@ -1,12 +1,12 @@
 import { getOpenSeaUrlForNft } from '@echo/model/helpers/nft/get-open-sea-url-for-nft'
-import { CHAIN_BLAST, TESTNET_CHAIN_SEPOLIA } from '@echo/utils/constants/chain-names'
+import { CHAIN_ETHEREUM, TESTNET_CHAIN_SEPOLIA } from '@echo/utils/constants/chain-names'
 import type { ChainName } from '@echo/utils/types/chain-name'
 import { describe, expect, it } from '@jest/globals'
 import { toLower } from 'ramda'
 
 describe('helpers - nft - getOpenSeaUrlForNft', () => {
   it('returns the mainnet URL', () => {
-    const chain = CHAIN_BLAST
+    const chain = CHAIN_ETHEREUM
     const address = toLower('0x320e2fa93A4010ba47edcdE762802374bac8d3F7')
     const tokenId = 1376
     const url = getOpenSeaUrlForNft({ address, chain }, tokenId)
