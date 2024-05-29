cc5c444562cb024c19ba9fcd885b313046077aa7
Johnny Cage Wins <2409861+0xjohnnycagewins@users.noreply.github.com>
2024-05-29 18:15:33 -0400

    fixed sort

--- lib/ui/src/helpers/nft/sort/sort-nfts-by-collection.ts
+++ lib/ui/src/helpers/nft/sort/sort-nfts-by-collection.ts
@@ -1,8 +1,9 @@
 import { nftByCollectionComparator } from '@echo/model/helpers/nft/nft-by-collection-comparator'
 import { nftByTokenIdComparator } from '@echo/model/helpers/nft/nft-by-token-id-comparator'
 import type { Nft } from '@echo/model/types/nft'
-import { pipe, sort } from 'ramda'
+import { sortWith } from 'ramda'
 
 export function sortNftsByCollection(nfts: Nft[]) {
-  return pipe<[Nft[]], Nft[], Nft[]>(sort(nftByTokenIdComparator), sort(nftByCollectionComparator))(nfts)
+  // return sort(nftByCollectionComparator, nfts)
+  return sortWith([nftByCollectionComparator, nftByTokenIdComparator], nfts)
 }
