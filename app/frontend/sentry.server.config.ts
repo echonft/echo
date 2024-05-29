d683f751be87fa66806bcfcf0ff2d82cdd01ca3f
Johnny Cage Wins <2409861+0xjohnnycagewins@users.noreply.github.com>
2024-05-29 17:12:01 -0400

    enabling Sentry in prod only

--- app/frontend/sentry.server.config.ts
+++ app/frontend/sentry.server.config.ts
@@ -2,9 +2,12 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
+import { isCi } from '@echo/utils/constants/is-ci'
+import { isProd } from '@echo/utils/constants/is-prod'
 import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
+  enabled: !isCi && isProd,
   dsn: 'https://90f90a5ace372a2805407eeeb7d7fc15@o4506149604098048.ingest.us.sentry.io/4506149609472000',
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
