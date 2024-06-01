1561f33de6bc03ff8a9ccb03a7722dd220fdc9b5
Johnny Cage Wins <2409861+0xjohnnycagewins@users.noreply.github.com>
2024-05-31 23:51:02 -0400

    added pinata gateway

--- lib/ui/src/helpers/add-picture-size-to-url.ts
+++ lib/ui/src/helpers/add-picture-size-to-url.ts
@@ -1,4 +1,5 @@
 import type { PictureSize } from '@echo/ui/types/picture-size'
+import { isDev } from '@echo/utils/constants/is-dev'
 import { errorMessage } from '@echo/utils/helpers/error-message'
 import { removeQueryFromUrl } from '@echo/utils/helpers/remove-query-from-url'
 import { pinoLogger } from '@echo/utils/services/pino-logger'
@@ -25,17 +26,23 @@ export function addPictureSizeToUrl(url: Nullable<string>, size: PictureSize): s
     if (hostname.includes('nft-cdn.alchemy.com')) {
       return `https://res.cloudinary.com/alchemyapi/image/upload/w_${size}/scaled${urlObject.pathname}`
     }
-    // TODO pay for pinata?
-    // if (hostname.includes('ipfs.io')) {
-    //   const match = urlObject.pathname.match(/ipfs\/([^/]+)\/?/)
-    //   if (match) {
-    //     const url = `https://beige-quick-seahorse-333.mypinata.cloud/ipfs/${match[1]}?img-width=${size}`
-    //     if (isDev) {
-    //       return `${url}&pinataGatewayToken=${process.env.NEXT_PUBLIC_PINATA_GATEWAY_KEY}`
-    //     }
-    //     return url
-    //   }
-    // }
+    if (hostname.includes('ipfs.io')) {
+      const match = urlObject.pathname.match(/ipfs\/([^/]+)\/?/)
+      if (match) {
+        const url = `https://pinata.echonft.xyz/ipfs/${match[1]}?img-width=${size}`
+        if (isDev) {
+          return `${url}&pinataGatewayToken=${process.env.NEXT_PUBLIC_PINATA_GATEWAY_KEY}`
+        }
+        return url
+      }
+    }
+    if (hostname.startsWith('ipfs://')) {
+      const url = `https://pinata.echonft.xyz/ipfs/${hostname.slice(6)}?img-width=${size}`
+      if (isDev) {
+        return `${url}&pinataGatewayToken=${process.env.NEXT_PUBLIC_PINATA_GATEWAY_KEY}`
+      }
+      return url
+    }
     return url
   } catch (err) {
     pinoLogger.error(`addPictureSizeToUrl error: ${errorMessage(err)}`)
