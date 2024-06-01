9d7501700133e290722ad4b8d8a372187aa6cc67
Gabriel Cartier <4060669+GabrielCartier@users.noreply.github.com>
2024-05-31 23:19:43 -0400

    updated schema for discord info

--- app/frontend/src/lib/validators/discord-profile-schema.ts
+++ app/frontend/src/lib/validators/discord-profile-schema.ts
@@ -1,34 +1,40 @@
 import { always, isNil, when } from 'ramda'
 import { z } from 'zod'
 
-function removeUndefined<T>(value: T | undefined) {
+function removeUndefined<T>(value: T | undefined | null) {
   return when(isNil, always(null))(value) as T | null
 }
 export const discordProfileSchema = z.object({
   accent_color: z
     .number()
     .optional()
+    .nullable()
     .transform<number | null>(removeUndefined<number>),
   avatar: z
     .string()
     .optional()
+    .nullable()
     .transform<string | null>(removeUndefined<string>),
   avatar_decoration: z
     .string()
     .optional()
+    .nullable()
     .transform<string | null>(removeUndefined<string>),
   banner_color: z
     .string()
     .optional()
+    .nullable()
     .transform<string | null>(removeUndefined<string>),
   banner: z
     .string()
     .optional()
+    .nullable()
     .transform<string | null>(removeUndefined<string>),
   discriminator: z.string().min(1),
   global_name: z
     .string()
     .optional()
+    .nullable()
     .transform<string | null>(removeUndefined<string>),
   id: z.string().min(1),
   username: z.string().min(1)
