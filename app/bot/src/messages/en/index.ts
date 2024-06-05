d3f93110d6dbc6c8cf30b30378829c42c424e389
Gabriel Cartier <4060669+GabrielCartier@users.noreply.github.com>
2024-06-05 11:42:32 -0400

    added message to redeem

--- app/bot/src/messages/en/index.ts
+++ app/bot/src/messages/en/index.ts
@@ -28,12 +28,19 @@ export const messages = {
       message:
         '{{- sender}} made you an offer {{- receiver}}. You can use this thread to discuss it, and hopefully come' +
         ' to an agreement. Happy swapping!',
-      name: 'offer-{{timestamp}}'
+      name: 'offer-{{timestamp}}',
+      redeemable: {
+        single: '{{- redeemer}} still has NFTs to redeem. Once this is done, thread will close automatically.',
+        multiple:
+          '{{- sender}} and {{- receiver}} still have NFTs to redeem. Once this is done, thread will close automatically.'
+      }
     },
     update: {
       ACCEPTED: '@here {{- receiver}} accepted the offer! You can now execute the swap.',
       CANCELLED: '@here The offer was cancelled.',
-      EXPIRED: '@here The offer expired.',
+      EXPIRED:
+        "@here The offer expired.\n There are still NFTs in escrow, we'll close the thread once all NFTs are redemeed.",
+      REDEEMED: '@here ',
       REJECTED: '@here {{- receiver}} rejected the offer.',
       COMPLETED: '@here The swap is now completed. Congratulations to the both of you!'
     }
---
+++ app/bot/src/offer/post-escrow-message-if-needed.ts
@@ -0,0 +1,59 @@
+import { sendToThread } from '@echo/bot/helpers/send-to-thread'
+import { buildOfferLinkButton } from '@echo/bot/offer/build-offer-link-button'
+import { getOfferUpdatesByOfferId } from '@echo/firestore/crud/offer-update/get-offer-updates-by-offer-id'
+import { getUserByUsername } from '@echo/firestore/crud/user/get-user-by-username'
+import type { OfferThread } from '@echo/firestore/types/model/offer-thread/offer-thread'
+import { OFFER_STATE_ACCEPTED, OFFER_STATE_EXPIRED, OFFER_STATE_REJECTED } from '@echo/model/constants/offer-states'
+import type { Offer } from '@echo/model/types/offer'
+import { pinoLogger } from '@echo/utils/services/pino-logger'
+import { type AnyThreadChannel } from 'discord.js'
+import i18next from 'i18next'
+import { any, isEmpty, isNil, pathEq } from 'ramda'
+
+async function getUserDiscordId(username: string) {
+  const user = await getUserByUsername(username)
+  if (isNil(user)) {
+    throw Error(`offer receiver with username ${username} not found`)
+  }
+  return user.discord.id
+}
+
+function shouldPostEscrowMessage(offer: Offer) {
+  // On a rejection or expiry, you need to take out escrow
+  return offer.state === OFFER_STATE_REJECTED || offer.state === OFFER_STATE_EXPIRED
+}
+
+async function areBothPartiesInEscrow(offerId: string) {
+  const updates = await getOfferUpdatesByOfferId(offerId)
+  if (isEmpty(updates)) {
+    pinoLogger.error(`[OFFER ${offerId}] checked for escrow status but no updates were found`)
+    return false
+  }
+  // If there was an accepted state update, it means both parties are in escrow
+  return any(pathEq(OFFER_STATE_ACCEPTED, ['update', 'args', 'state']))(updates)
+}
+
+export async function postEscrowMessageIfNeeded(args: {
+  offerThread: OfferThread
+  thread: AnyThreadChannel<boolean>
+  offer: Offer
+}) {
+  const { offer, offerThread, thread } = args
+  if (shouldPostEscrowMessage(offer)) {
+    if (await areBothPartiesInEscrow(offerThread.offerId)) {
+      const sender = await getUserDiscordId(offer.sender.username)
+      const receiver = await getUserDiscordId(offer.receiver.username)
+      await sendToThread(thread, {
+        components: [buildOfferLinkButton(offer.slug)],
+        content: i18next.t('offer.thread.redeemable.multiple', { sender, receiver })
+      })
+    } else {
+      const sender = await getUserDiscordId(offer.sender.username)
+      await sendToThread(thread, {
+        components: [buildOfferLinkButton(offer.slug)],
+        content: i18next.t('offer.thread.redeemable.single', { redeemer: sender })
+      })
+    }
+    pinoLogger.info(`[OFFER ${offerThread.offerId}] posted escrow update to thread ${offerThread.guild.threadId}`)
+  }
+}
