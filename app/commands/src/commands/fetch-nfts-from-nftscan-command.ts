d19572affaf5bb3c87c1032121e7de3d443952d4
Johnny Cage Wins <2409861+0xjohnnycagewins@users.noreply.github.com>
2024-06-05 11:20:37 -0400

    rebased

--- app/commands/src/commands/fetch-nfts-from-nftscan-command.ts
+++ app/commands/src/commands/fetch-nfts-from-nftscan-command.ts
@@ -1,8 +1,7 @@
 import type { Command } from '@echo/commands/types/command'
 import { getAllNftsByAccount } from '@echo/nft-scan/services/get-all-nfts-by-account'
-import { CHAIN_ETHEREUM } from '@echo/utils/constants/chains/chains'
+import { getChains } from '@echo/utils/helpers/chains/get-chains'
 import { errorMessage } from '@echo/utils/helpers/error-message'
-import { getChains } from '@echo/utils/helpers/get-chains'
 import type { ChainName } from '@echo/utils/types/chain-name'
 import type { HexString } from '@echo/utils/types/hex-string'
 import { formatWalletAddress } from '@echo/web3/helpers/format-wallet-address'
@@ -34,7 +33,7 @@ export const fetchNftsFromNftscanCommand: Command = {
           describe: 'chain',
           type: 'string',
           choices: getChains(),
-          default: CHAIN_ETHEREUM,
+          default: 'ethereum',
           coerce: (arg) => arg as ChainName
         }
       })
