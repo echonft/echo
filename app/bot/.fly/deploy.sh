15092e94c9e0b9af7602464725abfa95b66ecbdc
Johnny Cage Wins <2409861+0xjohnnycagewins@users.noreply.github.com>
2024-06-05 19:18:34 -0400

    better depoyment for bot + contract-listener

---
+++ app/bot/.fly/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+if [ "$1" == "dev" ]; then
+  app="echo-bot-dev"
+  machine_id="e784e469a63678"
+elif [ "$1" == "prod" ]; then
+  app="echobot"
+  machine_id="784e9d4c271418"
+else
+  exit 1
+fi
+
+git_sha=$(git rev-parse --short HEAD)
+id="${app}:${git_sha}"
+docker buildx build --platform=linux/amd64 --target=bot -t registry.fly.io/${id} ${dir}/../../../
+docker push registry.fly.io/${id}
+flyctl deploy -i registry.fly.io/${id} --local-only --app=${app} --only-machines=${machine_id} --image-label=${git_sha} --label="version=${git_sha}"
