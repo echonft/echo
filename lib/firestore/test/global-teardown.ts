b9a2264ac4977f033461421b2e1fc0a7250bde79
Johnny Cage Wins <2409861+0xjohnnycagewins@users.noreply.github.com>
2024-06-10 18:14:52 -0400

    firestore tests fix

--- lib/firestore/test/global-teardown.ts
+++
@@ -1,7 +0,0 @@
-// noinspection JSUnusedGlobalSymbols
-import 'tsconfig-paths/register'
-import { terminateFirestore } from '@echo/firestore/services/terminate-firestore'
-
-export default async function () {
-  await terminateFirestore()
-}
