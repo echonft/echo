163cd9a85f483ac521f36d6b2456b7efd7acf0c7
Johnny Cage Wins <2409861+0xjohnnycagewins@users.noreply.github.com>
2024-06-10 17:59:10 -0400

    fixed bot tests

--- lib/utils/src/helpers/get-base-url.ts
+++ lib/utils/src/helpers/get-base-url.ts
@@ -1,7 +1,8 @@
 import { isDev } from '@echo/utils/constants/is-dev'
+import { isTest } from '@echo/utils/constants/is-test'
 
 export function getBaseUrl() {
-  if (isDev) {
+  if (isDev || isTest) {
     return 'http://localhost:3000'
   }
   return process.env.NEXT_PUBLIC_VERCEL_URL
