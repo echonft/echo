d9cf747082442f75e3d43aa234d10b79c7f1e75d
Johnny Cage Wins <2409861+0xjohnnycagewins@users.noreply.github.com>
2024-06-10 13:30:59 -0400

    added sentry auth token

--- app/frontend/scripts/build.sh
+++ app/frontend/scripts/build.sh
@@ -57,9 +57,11 @@ gcloud config set project "${project_id}"
 auth_discord_id=$(gcloud secrets versions access 'latest' --secret="DISCORD_CLIENT_ID" --project=echo-dev-fallback)
 auth_discord_secret=$(gcloud secrets versions access 'latest' --secret="DISCORD_CLIENT_SECRET" --project=echo-dev-fallback)
 auth_secret=$(gcloud secrets versions access 'latest' --secret="AUTH_SECRET" --project=echo-dev-fallback)
+sentry_auth_token=$(gcloud secrets versions access 'latest' --secret="SENTRY_AUTH_TOKEN" --project=echo-dev-fallback)
 
 # build with the env vars
 AUTH_SECRET="${auth_secret}" \
 AUTH_DISCORD_ID="${auth_discord_id}" \
 AUTH_DISCORD_SECRET="${auth_discord_secret}" \
+SENTRY_AUTH_TOKEN="${sentry_auth_token}" \
  pnpm exec turbo sentry:sourcemaps:upload --filter=@echo/frontend
