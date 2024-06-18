c2e7b5d421bb8810c460ce04bd951533dd107155
Gabriel Cartier <4060669+GabrielCartier@users.noreply.github.com>
2024-06-18 15:55:56 -0400

    updated task

--- app/commands/src/commands/update-users-nfts-command.ts
+++ app/commands/src/commands/update-users-nfts-command.ts
@@ -1,12 +1,8 @@
 import { getLogger } from '@echo/commands/helpers/get-logger'
 import type { Command } from '@echo/commands/types/command'
-import { updateUserNfts } from '@echo/firestore-functions/tasks/nft/update-user-nfts'
-import { getUserById } from '@echo/firestore/crud/user/get-user-by-id'
-import { getAllWallets } from '@echo/firestore/crud/wallet/get-all-wallets'
+import { getAllUsers } from '@echo/firestore/crud/user/get-all-users'
 import { initializeFirebase } from '@echo/firestore/services/initialize-firebase'
-import { terminateFirestore } from '@echo/firestore/services/terminate-firestore'
-import { errorMessage } from '@echo/utils/helpers/error-message'
-import { isNil } from 'ramda'
+import { updateNftsForUser } from '@echo/tasks/update-nfts-for-user'
 
 /**
  *  Fetch all the NFTs for all users in the DB.
@@ -15,30 +11,19 @@ export const updateUsersNftsCommand: Command = {
   name: 'update-users-nfts',
   execute: async function () {
     const logger = getLogger().child({ command: 'update-users-nfts' })
+    logger.info('Updating all users NFTs')
     try {
-      logger.info('Updating all users NFTs')
-      try {
-        await initializeFirebase()
-        const wallets = await getAllWallets()
-        for (const wallet of wallets) {
-          try {
-            const foundUser = await getUserById(wallet.userId)
-            if (!isNil(foundUser)) {
-              try {
-                await updateUserNfts(foundUser, [wallet], logger)
-              } catch (e) {
-                logger.error(`error upating user ${wallet.userId} NFTs: ${errorMessage(e)}`)
-              }
-            }
-          } catch (e) {
-            logger.error(`error getting user ${wallet.userId}: ${errorMessage(e)}`)
-          }
+      await initializeFirebase()
+      const users = await getAllUsers()
+      for (const user of users) {
+        try {
+          await updateNftsForUser({ user, logger })
+        } catch (err) {
+          logger.error({ err, user }, 'error upating user NFTs')
         }
-      } catch (err) {
-        logger.error('error updating users NFT')
       }
-    } finally {
-      await terminateFirestore()
+    } catch (err) {
+      logger.error('error updating users NFT')
     }
   }
 }
