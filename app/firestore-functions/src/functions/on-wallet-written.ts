c9074be26cc29a5eb27ec68d001443dfd75d1849
Johnny Cage Wins <2409861+0xjohnnycagewins@users.noreply.github.com>
2024-06-25 23:46:42 -0400

    added logger to firestore-functions + fixes

--- app/firestore-functions/src/functions/on-wallet-written.ts
+++ app/firestore-functions/src/functions/on-wallet-written.ts
@@ -1,36 +1,36 @@
+import { getLogger } from '@echo/firestore-functions/helper/get-logger'
 import { setMaxInstances } from '@echo/firestore-functions/helper/set-max-instances'
 import { getUserById } from '@echo/firestore/crud/user/get-user-by-id'
 import { getDocumentSnapshotData } from '@echo/firestore/helpers/crud/document/get-document-snapshot-data'
 import type { WalletDocumentData } from '@echo/firestore/types/model/wallet/wallet-document-data'
 import { removeNftsForWallet } from '@echo/tasks/remove-nfts-for-wallet'
 import { updateNftsForWallet } from '@echo/tasks/update-nfts-for-wallet'
-import { errorMessage } from '@echo/utils/helpers/error-message'
 import { DocumentSnapshot } from 'firebase-admin/firestore'
-import { error, info } from 'firebase-functions/logger'
 import { onDocumentWritten } from 'firebase-functions/v2/firestore'
 import { isNil } from 'ramda'
 
 export const onWalletWritten = onDocumentWritten(
   setMaxInstances({ document: 'wallets/{id}', timeoutSeconds: 540 }),
   async (event) => {
+    const logger = getLogger().child({ fn: 'onWalletWritten' })
     const change = event.data
     if (!isNil(change)) {
       if (change.after.exists) {
         // wallet was created
         const wallet = getDocumentSnapshotData<WalletDocumentData>(change.after as DocumentSnapshot<WalletDocumentData>)
         if (!isNil(wallet)) {
-          info(`wallet ${JSON.stringify(wallet)} was added`)
+          logger.info({ wallet }, 'wallet was added')
           try {
             const foundUser = await getUserById(wallet.userId)
             if (!isNil(foundUser)) {
               try {
-                await updateNftsForWallet({ wallet })
-              } catch (e) {
-                error(`error upating user ${wallet.userId} NFTs: ${errorMessage(e)}`)
+                await updateNftsForWallet({ wallet, logger })
+              } catch (err) {
+                logger.error({ err, wallet }, 'error updating NFTs for wallet')
               }
             }
-          } catch (e) {
-            error(`error getting user ${wallet.userId}: ${errorMessage(e)}`)
+          } catch (err) {
+            logger.error({ err, user: { id: wallet.userId } }, 'error getting user')
           }
         }
       } else if (change.before.exists) {
@@ -39,11 +39,11 @@ export const onWalletWritten = onDocumentWritten(
           change.before as DocumentSnapshot<WalletDocumentData>
         )
         if (!isNil(wallet)) {
-          info(`wallet ${JSON.stringify(wallet)} was deleted`)
+          logger.info({ wallet }, 'wallet was deleted')
           try {
-            await removeNftsForWallet({ wallet })
-          } catch (e) {
-            error(`error removing NFTs for wallet ${wallet.address}: ${errorMessage(e)}`)
+            await removeNftsForWallet({ wallet, logger })
+          } catch (err) {
+            logger.error({ err, wallet }, 'error removing NFTs for wallet')
           }
         }
       }
--- lib/utils/src/services/logger.ts
+++ lib/utils/src/services/logger.ts
@@ -3,8 +3,8 @@ import { isProd } from '@echo/utils/constants/is-prod'
 import { isTest } from '@echo/utils/constants/is-test'
 import type { LoggerOptions } from '@echo/utils/types/logger-options'
 import type { LoggerSerializer } from '@echo/utils/types/logger-serializer'
-import pino from 'pino'
-import { assoc, equals, is, isNil, mergeAll, mergeLeft, pipe } from 'ramda'
+import pino, { type DestinationStream } from 'pino'
+import { always, assoc, equals, is, isNil, mergeAll, mergeLeft, pipe, unless } from 'ramda'
 
 function getSerializers(serializers?: LoggerSerializer | LoggerSerializer[]): LoggerSerializer {
   const baseSerializer: LoggerSerializer = {
@@ -19,29 +19,35 @@ function getSerializers(serializers?: LoggerSerializer | LoggerSerializer[]): Lo
   return mergeLeft(baseSerializer, serializers)
 }
 
-export function getBaseLogger(name: string, options?: LoggerOptions) {
-  return pino({
-    enabled: options?.override?.enabled ?? (!isCI && !isTest),
-    level: options?.override?.level ?? (isProd ? 'info' : 'trace'),
-    name,
-    formatters: {
-      level(label, _number) {
-        return { level: label }
-      }
+export function getBaseLogger(name: string, options?: LoggerOptions, stream?: DestinationStream | undefined) {
+  return pino(
+    {
+      enabled: options?.override?.enabled ?? (!isCI && !isTest),
+      level: options?.override?.level ?? (isProd ? 'info' : 'trace'),
+      name,
+      formatters: {
+        level(label, _number) {
+          return { level: label }
+        }
+      },
+      mixin(mergeObject: object, _level: number) {
+        const baseMergeObject = pipe<[object], object, object, object>(
+          assoc('env', process.env.ENV),
+          assoc('node_env', process.env.NODE_ENV),
+          unless<object, object>(
+            always(Boolean(options?.hideNetwork)),
+            assoc('network', equals(process.env.NEXT_PUBLIC_IS_TESTNET, '1') ? 'testnet' : 'mainnet')
+          )
+        )(mergeObject)
+        if (!isNil(options) && !isNil(options.baseMergeObject)) {
+          return mergeLeft(options.baseMergeObject, baseMergeObject)
+        }
+        return baseMergeObject
+      },
+      serializers: getSerializers(options?.serializers),
+      base: undefined,
+      timestamp: false
     },
-    mixin(mergeObject: object, _level: number) {
-      const baseMergeObject = pipe<[object], object, object, object>(
-        assoc('env', process.env.ENV),
-        assoc('node_env', process.env.NODE_ENV),
-        assoc('network', equals(process.env.NEXT_PUBLIC_IS_TESTNET, '1') ? 'testnet' : 'mainnet')
-      )(mergeObject)
-      if (!isNil(options) && !isNil(options.baseMergeObject)) {
-        return mergeLeft(options.baseMergeObject, baseMergeObject)
-      }
-      return baseMergeObject
-    },
-    serializers: getSerializers(options?.serializers),
-    base: undefined,
-    timestamp: false
-  })
+    stream
+  )
 }
