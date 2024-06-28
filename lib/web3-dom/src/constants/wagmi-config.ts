18583f671e846133bfd8102655957304dfee5f92
Johnny Cage Wins <2409861+0xjohnnycagewins@users.noreply.github.com>
2024-06-28 13:15:37 -0400

    added back client

--- lib/web3-dom/src/constants/wagmi-config.ts
+++ lib/web3-dom/src/constants/wagmi-config.ts
@@ -1,8 +1,9 @@
 import { getChainId } from '@echo/utils/helpers/chains/get-chain-id'
 import { getSupportedChains } from '@echo/utils/helpers/chains/get-supported-chains'
+import { getWalletClient } from '@echo/web3-dom/helpers/get-wallet-client'
 import { getViemChainById } from '@echo/web3/helpers/chain/get-viem-chain-by-id'
-import { always, fromPairs, juxt, map, type NonEmptyArray, pipe, prop } from 'ramda'
-import { type Chain, http, type Transport } from 'viem'
+import { map, type NonEmptyArray, pipe } from 'ramda'
+import { type Chain } from 'viem'
 import { type Config, createConfig } from 'wagmi'
 import { injected } from 'wagmi/connectors'
 
@@ -10,11 +11,11 @@ const chains = pipe(getSupportedChains, map(pipe(getChainId, getViemChainById)))
 export const wagmiConfig: Config = createConfig({
   connectors: [injected()],
   chains,
-  transports: pipe<[NonEmptyArray<Chain>], [number, Transport][], Record<(typeof chains)[number]['id'], Transport>>(
-    map(juxt([prop('id'), always(http())])),
-    fromPairs<number, Transport>
-  )(chains)
-  // client({ chain }) {
-  //   return getWalletClient(chain)
-  // }
+  // transports: pipe<[NonEmptyArray<Chain>], [number, Transport][], Record<(typeof chains)[number]['id'], Transport>>(
+  //   map(juxt([prop('id'), always(http())])),
+  //   fromPairs<number, Transport>
+  // )(chains)
+  client({ chain }) {
+    return getWalletClient(chain)
+  }
 })
