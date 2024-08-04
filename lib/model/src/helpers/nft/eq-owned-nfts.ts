276b45f91485dba1e1e21b18220d9f88ea820474
Johnny Cage Wins <2409861+0xjohnnycagewins@users.noreply.github.com>
2024-08-04 13:08:32 -0400

    addressed PR comments

--- lib/model/src/helpers/nft/eq-owned-nfts.ts
+++ lib/model/src/helpers/nft/eq-owned-nfts.ts
@@ -1,9 +1,7 @@
-import { eqNft } from '@echo/model/helpers/nft/eq-nft'
-import { eqOwnedNftOwner } from '@echo/model/helpers/nft/eq-owned-nft-owner'
+import { eqOwnedNft } from '@echo/model/helpers/nft/eq-owned-nft'
 import type { OwnedNft } from '@echo/model/types/nft'
 import { eqListContentWith } from '@echo/utils/fp/eq-list-content-with'
-import { allPass } from 'ramda'
 
 export function eqOwnedNfts(nftsA: OwnedNft[], nftsB: OwnedNft[]): boolean {
-  return eqListContentWith<OwnedNft>(allPass([eqOwnedNftOwner, eqNft]))(nftsA, nftsB)
+  return eqListContentWith<OwnedNft>(eqOwnedNft)(nftsA, nftsB)
 }
--- lib/ui/src/helpers/nft/sort/sort-nfts-by-owner.ts
+++ lib/ui/src/helpers/nft/sort/sort-nfts-by-owner.ts
@@ -1,8 +1,8 @@
 import { nftByTokenIdComparator } from '@echo/model/helpers/nft/nft-by-token-id-comparator'
-import { ownedNftComparator } from '@echo/model/helpers/nft/owned-nft-comparator'
+import { nftOwnerComparator } from '@echo/model/helpers/nft/nft-owner-comparator'
 import type { OwnedNft } from '@echo/model/types/nft'
 import { sortWith } from 'ramda'
 
 export function sortNftsByOwner(nfts: OwnedNft[]): OwnedNft[] {
-  return sortWith([ownedNftComparator, nftByTokenIdComparator], nfts)
+  return sortWith([nftOwnerComparator, nftByTokenIdComparator], nfts)
 }
