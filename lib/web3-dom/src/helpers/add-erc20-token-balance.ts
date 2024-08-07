3a3bf3ac228c666c4219978e9f4e3c2e76fc1a20
Gabriel Cartier <4060669+GabrielCartier@users.noreply.github.com>
2024-08-07 18:24:09 -0400

    re-added the format

--- lib/web3-dom/src/helpers/add-erc20-token-balance.ts
+++ lib/web3-dom/src/helpers/add-erc20-token-balance.ts
@@ -2,7 +2,7 @@ import type { ERC20Token } from '@echo/model/types/erc20-token'
 import type { OwnedERC20Token } from '@echo/model/types/owned-erc20-token'
 import type { Wallet } from '@echo/model/types/wallet'
 import { assoc } from 'ramda'
-import { erc20Abi, type WalletClient } from 'viem'
+import { erc20Abi, formatUnits, type WalletClient } from 'viem'
 import { readContract } from 'viem/actions'
 
 export interface AddErc20TokenBalanceArgs {
@@ -21,5 +21,6 @@ export async function addERC20TokenBalance(args: AddErc20TokenBalanceArgs): Prom
     address: token.contract,
     args: [address]
   })
-  return assoc('balance', Number(balance), token)
+
+  return assoc('balance', Number(formatUnits(balance, token.decimals)), token)
 }
