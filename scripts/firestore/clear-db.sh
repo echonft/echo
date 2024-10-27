b0282e50ce69a53f706d79f4decb628db55ca476
Johnny Cage Wins <2409861+0xjohnnycagewins@users.noreply.github.com>
2024-10-27 18:07:25 -0400

    updated firestore script + indexes

--- scripts/firestore/clear-db.sh
+++ scripts/firestore/clear-db.sh
@@ -14,8 +14,18 @@ ENV=$(whiptail --default-item=development --notags --menu "Pick an environment" 
 if [ "$ENV" = "development" ] || [ "$ENV" = "staging" ] || [ "$ENV" = "production" ]; then
   printf "\e[36mClearing database of %s...\n\e[0m" "${ENV}"
   firebase use "${ENV}"
-  firebase firestore:databases:delete "(default)" --force
-  sleep 300
+#  firebase firestore:databases:delete "(default)" --force
+#  printf "\e[35m\nDeleted % database, waiting 2 minutes...\n\e[0m" "${ENV}"
+#  # Re-enable exit on error
+#  set -e
+#  duration=120
+#  while [ "$duration" -gt 0 ]
+#  do
+#    printf "\e[35m\r%02d:%02d\e[0m" $((duration/60)) $((duration%60))
+#    sleep 1
+#    duration=$((duration-1))
+#  done
+#  sleep 300
   firebase firestore:databases:create "(default)" --location=us-central1
   firebase deploy --only firestore:rules
   firebase deploy --only firestore:index
