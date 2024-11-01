22ffad0bc76293c2f2bf9a2b602e6157de52c375
Gabriel Cartier <4060669+GabrielCartier@users.noreply.github.com>
2024-11-01 18:21:19 +0700

    rebase

--- lib/model/src/helpers/chain/chains-for-virtual-machine.ts
+++ lib/model/src/helpers/chain/chains-for-virtual-machine.ts
@@ -5,8 +5,10 @@ import { network, Network } from '@echo/utils/constants/network'
 import { both, filter, map, type NonEmptyArray, pipe, prop, propEq, values } from 'ramda'
 
 export function chainsForVirtualMachine(vm: VirtualMachine): NonEmptyArray<Chain> {
+  // FIXME Typing is broken here
   return pipe(
     values,
+    filter(prop('supported')),
     filter(
       both<[ChainProps]>(propEq<Network, 'network'>(network(), 'network'), propEq<VirtualMachine, 'vm'>(vm, 'vm'))
     ),
