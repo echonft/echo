cd084403e38ccca12db823af9571ebc2bd74fe7f
Gabriel Cartier <4060669+GabrielCartier@users.noreply.github.com>
2024-11-20 18:34:44 +0700

    fixed nft schema to support ipfs string

--- lib/model/src/validators/nft-schema.ts
+++ lib/model/src/validators/nft-schema.ts
@@ -18,7 +18,7 @@ export const nftSchema = object({
   collection: collectionSchema.pick({ contract: true, name: true, slug: true, totalSupply: true }),
   name: string().min(1),
   owner: userSchema.optional(),
-  pictureUrl: string().url().optional(), // TODO add without query params
+  pictureUrl: string().url().optional().or(string().optional()), // TODO add without query params
   tokenId: number().int().positive(),
   type: nftTokenTypeSchema
 })
