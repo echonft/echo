64c58c9fc7606dd5f3939656c6570d8c09cf8e4a
Gabriel Cartier <4060669+GabrielCartier@users.noreply.github.com>
2024-11-26 17:03:24 +0800

    removed creating listing with NFT for now

--- lib/ui/src/pages/profile/profile-items-panel.tsx
+++ lib/ui/src/pages/profile/profile-items-panel.tsx
@@ -1,13 +1,7 @@
-'use client'
-
 import type { OwnedNft } from '@echo/model/types/nft'
-import { frontendRoutes } from '@echo/routing/constants/frontend-routes'
-import { SelectableNftsWithFilters } from '@echo/ui/components/nft/filters/selectable-nfts-with-filters'
-import { NftAction } from '@echo/ui/constants/nft-actions'
+import { NftsWithFilters } from '@echo/ui/components/nft/filters/nfts-with-filters'
 import { TabPanel } from '@headlessui/react'
 import { clsx } from 'clsx'
-import { useRouter } from 'next/navigation'
-import type { NonEmptyArray } from 'ramda'
 import type { FunctionComponent } from 'react'
 
 interface Props {
@@ -16,18 +10,10 @@ interface Props {
 }
 
 export const ProfileItemsPanel: FunctionComponent<Props> = ({ nfts, show }) => {
-  const router = useRouter()
   if (show) {
     return (
       <TabPanel className={clsx('outline-none')}>
-        <SelectableNftsWithFilters
-          nfts={nfts}
-          sortBy={'collection'}
-          action={NftAction.Listing}
-          onSelectionAction={(selection) => {
-            router.push(frontendRoutes.listing.create.withQuery({ items: selection as NonEmptyArray<OwnedNft> }).get())
-          }}
-        />
+        <NftsWithFilters nfts={nfts} sortBy={'collection'} options={{ owner: { hide: true } }} />
       </TabPanel>
     )
   }
