ac80137877c555e7c7035f43b93a6de3fd0f143a
Gabriel Cartier <4060669+GabrielCartier@users.noreply.github.com>
2024-11-26 15:27:37 +0800

    fixed stack layout

--- lib/ui/src/components/base/stack/layout/stack-layout.tsx
+++ lib/ui/src/components/base/stack/layout/stack-layout.tsx
@@ -9,24 +9,41 @@ interface Props {
 
 export const StackLayout: FunctionComponent<PropsWithChildren<Props>> = ({ children, onClick }) => {
   return (
-    <div
-      className={clsx(
-        'rounded-2xl',
-        'w-[13.5rem]',
-        'card-height',
-        'overflow-clip',
-        'bg-stack',
-        // 'bg-[length:208px_310px]',
-        'bg-no-repeat',
-        // 'bg-origin-border',
-        'group',
-        'transition ease-in-out',
-        'outline-none',
-        !isNil(onClick) && 'cursor-pointer'
-      )}
-      onClick={onClick}
-    >
-      <div className={clsx('translate-x-0.5', 'translate-y-[1px]')}>{children}</div>
+    <div className={clsx('relative')}>
+      <div
+        className={clsx(
+          'rounded-2xl',
+          'w-[12.625rem]',
+          'card-height',
+          'overflow-clip',
+          'border',
+          'border-solid',
+          'border-white/10',
+          'bg-dark-500',
+          'group',
+          'transition ease-in-out',
+          'outline-none',
+          !isNil(onClick) && 'cursor-pointer'
+        )}
+        onClick={onClick}
+      >
+        {children}
+      </div>
+      <div
+        className={clsx(
+          'absolute',
+          'w-[12.625rem]',
+          'card-height',
+          'rounded-2xl',
+          'top-1',
+          'left-2',
+          'rotate-[2deg]',
+          '-z-10',
+          'border-white/10',
+          'border',
+          'bg-dark-250'
+        )}
+      />
     </div>
   )
 }
