4d1a8b268f327d7c8c7f6d61b2c716f4a180b104
Johnny Cage Wins <2409861+0xjohnnycagewins@users.noreply.github.com>
2024-12-04 18:11:39 -0500

    updated indexes

--- scripts/firestore/deploy-indexes.sh
+++ scripts/firestore/deploy-indexes.sh
@@ -2,7 +2,7 @@
 
 ENV=$(whiptail --default-item=development --notags --menu "Pick an environment" 10 30 2 \
 "development" "Development" \
-"production" "Production" 3>&1 1>&2 2>&3
+"production" "Production" 3>&1 1>&2 2>&3)
 
 if [ "$ENV" = "development" ] || [ "$ENV" = "production" ]; then
   printf "\e[36mDeploying Firestore indexes to %s...\n\e[0m" "$ENV"
