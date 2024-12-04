06e9c679581c121ac6191fe959d8f3f6b53383fd
Johnny Cage Wins <2409861+0xjohnnycagewins@users.noreply.github.com>
2024-12-04 18:16:04 -0500

    updated meta picture

--- app/frontend/src/lib/constants/metadata.ts
+++ app/frontend/src/lib/constants/metadata.ts
@@ -1,4 +1,4 @@
 export const metadataTitle = 'Echo'
 export const metadataDescription =
   'The first ever NFT swapping solution on SEI. Go ahead and be one of the first to experience it!'
-export const metadataImageUrl = 'https://storage.googleapis.com/echo-dev-public/blast_echo.png?alt=media'
+export const metadataImageUrl = 'https://storage.googleapis.com/echo-dev-public/meta-tag.png?alt=media'
